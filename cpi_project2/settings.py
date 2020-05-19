aa4016e6db7d9c671aa8a7f63712ff600654b49c
Matje1979 <45924019+Matje1979@users.noreply.github.com>
2020-05-20 01:42:25 +0200

    Update settings.py

--- cpi_project2/settings.py
+++ cpi_project2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&3z8depz)w(mm$tbw!rlpr5n(svxk*@#(i)86x=-adlj#o4l4f'
+SECRET_KEY = '****************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
